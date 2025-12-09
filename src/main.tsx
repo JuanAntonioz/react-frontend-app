cba87134b8b52b90e1083a79f5069b05e70471c4
Antonioz <1317580+Astronatus@users.noreply.github.com>
2025-12-08 23:06:26 -0500

    chore(react): remove StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { AppRoutes } from './core/router/app-router';
 import { AuthProvider } from './core/auth';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <AuthProvider>
-      <AppRoutes />
-    </AuthProvider>
-  </StrictMode>,
+  <AuthProvider>
+    <AppRoutes />
+  </AuthProvider>,
 );
